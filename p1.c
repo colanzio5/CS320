056f37f48da7167534b52bb0fd1c5a09a6256312
Colin Casazza <Colanzio5@gmail.com>
2017-02-22 14:09:50 -0800

    p1.c

---
+++ p1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void)	{
+
+	
+	char input[100], inp;
+	char goo[
+	int foo = 0;
+	int i = 0;
+	int j = 0;
+
+	printf("Input: \n");
+	while(i < 21)	{
+		
+		if(inp == ( '_' || '?' || ':' || '/' || '&' || ' ' ))	{
+
+			inp = getchar();
+			input[j] = '%'; j++;
+			input[j] = (int)inp; ++j;
+
+	printf("Output:   %s\n", input);
+	printf("Output2:  %s\n", input2); 
+	
+	return 0;
+
+}
+
+
+
+
