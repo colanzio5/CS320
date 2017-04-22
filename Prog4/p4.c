ab627b878564aa1836d0a313979058b66eab805f
Colin Casazza <Colanzio5@gmail.com>
2017-04-22 16:15:43 -0700

    4/22/17

---
+++ Prog4/p4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+	char ch;
+	int count = 0;
+	FILE *fp;
+	fp = fopen(argv[1],"r");
+	
+	while((ch = fgetc(fp)) != EOF){
+		if( count < 20 ){
+			if(isalpha(ch)){
+				if(isupper(ch)){
+					printf("%c", tolower(ch));
+					count++;
+				}
+				else{ 
+					printf("%c", toupper(ch));
+					count++;
+				}
+			}
+
+			else	{
+				printf("%c", ch);
+				count++;
+			}	
+		}
+		if (count == 20) {
+			printf("\n");
+			count = 0;
+		}
+	}
+
+	fclose(fp);
+	return(0);	
+}
